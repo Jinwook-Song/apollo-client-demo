e08886bca467c26dafcbffe46707581c1750d9e0
jinwook song <wlsdnr129@naver.com>
2022-08-15 17:08:41 +0900

    apollo provider

--- src/routes/movies.tsx
+++ src/routes/movies.tsx
@@ -1,5 +1,55 @@
+import { gql, useApolloClient } from '@apollo/client';
+import { useEffect, useState } from 'react';
+
+type Movie = {
+  id: number;
+  url: string;
+  imdb_code: string;
+  title: string;
+  title_english: string;
+  title_long: string;
+  slug: string;
+  year: number;
+  rating: number;
+  runtime: number;
+  genres: string[];
+  summary?: string;
+  description_full: string;
+  synopsis: string;
+  yt_trailer_code: string;
+  language: string;
+  background_image: string;
+  background_image_original: string;
+  small_cover_image: string;
+  medium_cover_image: string;
+  large_cover_image: string;
+};
+
 function Movies() {
-  return <div>Movies</div>;
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const client = useApolloClient();
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          {
+            allMovies {
+              id
+              title
+            }
+          }
+        `,
+      })
+      .then((data) => setMovies(data.data.allMovies));
+  }, [client]);
+
+  return (
+    <div>
+      {movies.map((moive) => (
+        <li key={moive.id}>{moive.title}</li>
+      ))}
+    </div>
+  );
 }
 
 export default Movies;
